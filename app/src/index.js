f295217fdaca819911271ba1b56de2d0bf2f1ed7
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-06 18:22:45 -0400

    setup. api chosen

--- app/src/index.js
+++ app/src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-=======
->>>>>>> b5af9982fcb869b08f9a33881041f05e74195685
+import reducer from './reducers';
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
